9a7772b26c83c063c454fdf0a633f2e64632dbf1
Myles <bdsc82@gmail.com>
2020-02-03 19:45:11 -0600

    step 2 done

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-public class Employer extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
+@Entity
+public class Employer extends AbstractEntity{
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String location;
+
+
+    public Employer(@NotBlank @Size(min = 2, max = 100) String location){
+        this.location = location;
+
+    }
+
+    public Employer(){
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+         this.location = location;
+    }
 }
